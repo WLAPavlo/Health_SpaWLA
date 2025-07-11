7868b7fb357dc7b5005c20bfd9ba06d24b7fb4b6
Pavlo <pavlo.lu@thewhitelabelagency.com>
2025-07-11 16:15:12 +0300

    Commit Process

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -11,6 +11,8 @@ import objectFitImages from 'object-fit-images';
 // import { jarallax, jarallaxElement } from 'jarallax';
 // import ScrollOut from 'scroll-out';
 
+/* global google */
+
 /**
  * Import scripts from Custom Divi blocks
  */
@@ -210,6 +212,13 @@ $(window).on('load', function () {
   if ($preloader.length) {
     $preloader.addClass('preloader--hidden');
   }
+
+  /*
+   *  This function will render each map when the document is ready (page has loaded)
+   */
+  $('.event-map-container').each(function () {
+    render_map($(this));
+  });
 });
 
 /**
@@ -239,3 +248,100 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   }
 });
+
+// ACF Google Map JS code
+
+/*
+ *  This function will render a Google Map onto the selected jQuery element
+ */
+function render_map($el) {
+  // var
+  var $markers = $el.find('.marker');
+  // var styles = []; // Uncomment for map styling
+
+  // vars
+  var args = {
+    zoom: 16,
+    center: new google.maps.LatLng(0, 0),
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    scrollwheel: false,
+    // styles : styles // Uncomment for map styling
+  };
+
+  // create map
+  var map = new google.maps.Map($el[0], args);
+
+  // add a markers reference
+  map.markers = [];
+
+  // add markers
+  $markers.each(function () {
+    add_marker($(this), map);
+  });
+
+  // center map
+  center_map(map);
+}
+
+/*
+ *  This function will add a marker to the selected Google Map
+ */
+var infowindow;
+
+function add_marker($marker, map) {
+  // var
+  var latlng = new google.maps.LatLng(
+    $marker.attr('data-lat'),
+    $marker.attr('data-lng')
+  );
+
+  // create marker
+  var marker = new google.maps.Marker({
+    position: latlng,
+    map: map,
+    //icon: $marker.data('marker-icon') //uncomment if you use custom marker
+  });
+
+  // add to array
+  map.markers.push(marker);
+
+  // if marker contains HTML, add it to an infoWindow
+  if ($.trim($marker.html())) {
+    // create info window
+    infowindow = new google.maps.InfoWindow();
+
+    // show info window when marker is clicked
+    google.maps.event.addListener(marker, 'click', function () {
+      // Close previously opened infowindow, fill with new content and open it
+      infowindow.close();
+      infowindow.setContent($marker.html());
+      infowindow.open(map, marker);
+    });
+  }
+}
+
+/*
+ *  This function will center the map, showing all markers attached to this map
+ */
+function center_map(map) {
+  // vars
+  var bounds = new google.maps.LatLngBounds();
+
+  // loop through all markers and create bounds
+  $.each(map.markers, function (i, marker) {
+    var latlng = new google.maps.LatLng(
+      marker.position.lat(),
+      marker.position.lng()
+    );
+    bounds.extend(latlng);
+  });
+
+  // only 1 marker?
+  if (map.markers.length == 1) {
+    // set center of map
+    map.setCenter(bounds.getCenter());
+  } else {
+    // fit to bounds
+    map.fitBounds(bounds);
+  }
+}
--- templates/template-events.php
+++ templates/template-events.php
@@ -8,23 +8,20 @@ get_header(); ?>
     <div class="grid-container">
         <div class="grid-x grid-margin-x">
             <div class="cell">
-                <!-- Page Title -->
+                <!-- Page Title - Centered -->
                 <div class="events-page-header">
                     <h1 class="events-page-title">Upcoming events</h1>
                 </div>
 
-
+                <!-- Single Featured Events Stripe - Outside of posts loop -->
+                <div class="featured-events-stripe">
+                    <div class="stripe-content">
+                        FEATURED EVENTS - FEATURED EVENTS - FEATURED EVENTS - FEATURED EVENTS
+                    </div>
+                </div>
 
                 <!-- Events List -->
                 <div class="events-list">
-                    <!-- Events Info Stripe -->
-                    <?php
-                    $events_stripe_text = get_field('events_stripe_text', 'options');
-                    if ($events_stripe_text): ?>
-                        <div class="events-info-stripe">
-                            <p class="stripe-text"><?php echo esc_html($events_stripe_text); ?></p>
-                        </div>
-                    <?php endif; ?>
                     <?php
                     // Pagination setup
                     $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
@@ -42,121 +39,127 @@ get_header(); ?>
 
                     if ($events_query->have_posts()):
                         while ($events_query->have_posts()): $events_query->the_post();
-                            $event_month = get_field('event_month');
-                            $event_day = get_field('event_day');
-                            $month = get_field('month');
-                            $day = get_field('day');
+                            $two_month = get_field('two_month');
+                            $two_day = get_field('two_day');
                             $event_location = get_field('event_location');
                             $event_subtitle = get_field('event_subtitle');
-                            $event_description = get_field('event_description');
+                            $event_description = get_field('event_description_wysiwyg');
                             $event_link = get_field('event_link');
                             $event_logo = get_field('event_logo');
-                            $event_featured_image = get_field('event_featured_image');
+                            $post_featured_image = get_field('post_featured_image');
                             ?>
+
                             <div class="event-item">
-                                <!-- Event Date Block -->
-                                <div class="event-date-block">
-                                    <?php if ($month): ?>
-                                        <div class="event-month"><?php echo esc_html($month); ?></div>
-                                    <?php endif; ?>
-                                    <?php if ($day): ?>
-                                        <div class="event-day"><?php echo esc_html($day); ?></div>
-                                    <?php endif; ?>
-                                </div>
+                                <div class="event-item-wrapper">
+                                    <!-- Event Date Block - Left side, outside content -->
+                                    <div class="event-date-block">
+                                        <?php if ($two_month): ?>
+                                            <div class="event-month"><?php echo esc_html($two_month); ?></div>
+                                        <?php endif; ?>
+                                        <?php if ($two_day): ?>
+                                            <div class="event-day"><?php echo esc_html($two_day); ?></div>
+                                        <?php endif; ?>
+                                    </div>
 
-                                <!-- Event Content -->
-                                <div class="event-content">
-                                    <h2 class="event-title"><?php the_title(); ?></h2>
+                                    <!-- Event Content -->
+                                    <div class="event-content">
+                                        <h2 class="event-title"><?php the_title(); ?></h2>
 
-                                    <!-- Event Logo - перший після заголовка -->
-                                    <?php if ($event_logo): ?>
-                                        <div class="event-logo-section">
-                                            <img src="<?php echo esc_url($event_logo['url']); ?>"
-                                                 alt="<?php echo esc_attr($event_logo['alt'] ?: get_the_title()); ?>"
-                                                 class="event-logo">
-                                        </div>
-                                    <?php endif; ?>
+                                        <!-- Event Featured Image -->
+                                        <?php if ($post_featured_image): ?>
+                                            <div class="event-image">
+                                                <img src="<?php echo esc_url($post_featured_image['url']); ?>"
+                                                     alt="<?php echo esc_attr($post_featured_image['alt'] ?: get_the_title()); ?>">
+                                            </div>
+                                        <?php elseif (has_post_thumbnail()): ?>
+                                            <div class="event-image">
+                                                <?php the_post_thumbnail('large'); ?>
+                                            </div>
+                                        <?php endif; ?>
 
-                                    <!-- Event Featured Image - після логотипу -->
-                                    <?php if ($event_featured_image): ?>
-                                        <div class="event-image">
-                                            <img src="<?php echo esc_url($event_featured_image['url']); ?>"
-                                                 alt="<?php echo esc_attr($event_featured_image['alt'] ?: get_the_title()); ?>">
-                                        </div>
-                                    <?php endif; ?>
+                                        <!-- Event Description -->
+                                        <?php if ($event_description): ?>
+                                            <div class="event-description">
+                                                <?php echo wp_kses_post($event_description); ?>
+                                            </div>
+                                        <?php elseif (get_the_content()): ?>
+                                            <div class="event-description">
+                                                <?php the_content(); ?>
+                                            </div>
+                                        <?php endif; ?>
 
-                                    <!-- Event Description -->
-                                    <?php if ($event_description): ?>
-                                        <div class="event-description">
-                                            <?php echo wp_kses_post($event_description); ?>
-                                        </div>
-                                    <?php endif; ?>
-
-                                    <!-- Read More Button -->
-                                    <?php if ($event_link): ?>
+                                        <!-- Read More Button -->
                                         <div class="event-read-more-section">
-                                            <a href="<?php echo get_permalink(); ?>"
-                                               class="register-link" <!-- замінено клас тут -->
-                                            >
-                                            TO THE EVENT PAGE
-                                            </a>
-                                        </div>
-                                    <?php else: ?>
-                                        <div class="event-read-more-section">
-                                            <a href="<?php echo get_permalink(); ?>" class="register-link">
+                                            <a href="<?php echo get_permalink(); ?>" class="event-read-more-btn">
                                                 TO THE EVENT PAGE
                                             </a>
                                         </div>
-                                    <?php endif; ?>
 
-                                    <!-- Event Details Table - компактна в один ряд -->
-                                    <div class="event-details">
-                                        <div class="event-details-row">
-                                            <div class="event-detail-item">
-                                                <div class="detail-label">WHEN</div>
-                                                <div class="detail-value">
-                                                    <?php
-                                                    $event_year = get_field('event_year') ?: '2025';
-                                                    $event_time_start = get_field('event_time_start') ?: '18:00';
-                                                    $event_time_end = get_field('event_time_end') ?: '21:00';
-                                                    ?>
-                                                    <?php if ($event_month && $event_day): ?>
-                                                        <?php echo esc_html($event_month . ' ' . $event_day); ?>, <?php echo esc_html($event_year); ?><br>
-                                                    <?php endif; ?>
-                                                    <?php echo esc_html($event_time_start . ' - ' . $event_time_end); ?>
+                                        <!-- Event Details Table - 3 columns -->
+                                        <div class="event-details">
+                                            <div class="event-details-row">
+                                                <div class="event-detail-item">
+                                                    <div class="detail-label">WHEN</div>
+                                                    <div class="detail-value">
+                                                        <?php
+                                                        $event_year = get_field('event_year') ?: '2025';
+                                                        $event_time_start = get_field('event_time_start') ?: '16:30';
+                                                        $event_time_end = get_field('event_time_end') ?: '21:00';
+                                                        $when_full_date = get_field('when_full_date');
+                                                        $when_time_range = get_field('when_time_range');
+                                                        ?>
+                                                        <?php if ($when_full_date): ?>
+                                                            <?php echo esc_html($when_full_date); ?><br>
+                                                        <?php elseif ($event_month && $event_day): ?>
+                                                            <?php echo esc_html($event_month . ' ' . $event_day); ?>, <?php echo esc_html($event_year); ?><br>
+                                                        <?php endif; ?>
+                                                        <?php if ($when_time_range): ?>
+                                                            <?php echo esc_html($when_time_range); ?>
+                                                        <?php else: ?>
+                                                            <?php echo esc_html($event_time_start . ' - ' . $event_time_end); ?>
+                                                        <?php endif; ?>
+                                                    </div>
                                                 </div>
-                                            </div>
-                                            <div class="event-detail-item">
-                                                <div class="detail-label">WHERE</div>
-                                                <div class="detail-value">
-                                                    <?php
-                                                    $event_venue_name = get_field('event_venue_name') ?: 'Ravintolasali 8';
-                                                    $event_venue_address = get_field('event_venue_address') ?: '00100 HELSINKI';
-                                                    ?>
-                                                    <?php if ($event_location): ?>
-                                                        <?php echo esc_html($event_location); ?><br>
-                                                    <?php endif; ?>
-                                                    <?php echo esc_html($event_venue_name); ?><br>
-                                                    <?php echo esc_html($event_venue_address); ?>
+                                                <div class="event-detail-item">
+                                                    <div class="detail-label">WHERE</div>
+                                                    <div class="detail-value">
+                                                        <?php
+                                                        $event_venue_name = get_field('event_venue_name') ?: get_field('where_venue_name') ?: 'Biomedicum Helsinki 1';
+                                                        $event_venue_address = get_field('event_venue_address');
+                                                        $where_address_line1 = get_field('where_address_line1');
+                                                        $where_address_line2 = get_field('where_address_line2');
+                                                        ?>
+                                                        <?php echo esc_html($event_venue_name); ?><br>
+                                                        <?php if ($event_venue_address): ?>
+                                                            <?php echo esc_html($event_venue_address); ?>
+                                                        <?php elseif ($where_address_line1): ?>
+                                                            <?php echo esc_html($where_address_line1); ?><br>
+                                                            <?php if ($where_address_line2): ?>
+                                                                <?php echo esc_html($where_address_line2); ?>
+                                                            <?php endif; ?>
+                                                        <?php else: ?>
+                                                            Haartmaninkatu 8<br>00290 Helsinki
+                                                        <?php endif; ?>
+                                                    </div>
                                                 </div>
-                                            </div>
-                                            <div class="event-detail-item">
-                                                <div class="detail-label">ATTENDANCE</div>
-                                                <div class="detail-value">
-                                                    <?php
-                                                    $event_attendance_info = get_field('event_attendance_info') ?: 'The event is free, but seating is limited to 50.';
-                                                    $event_registration_link = get_field('event_registration_link');
-                                                    ?>
-                                                    <?php echo nl2br(esc_html($event_attendance_info)); ?><br>
-                                                    <?php if ($event_registration_link): ?>
-                                                        <a href="<?php echo esc_url($event_registration_link['url']); ?>"
-                                                           class="register-link"
-                                                           <?php if ($event_registration_link['target']): ?>target="<?php echo esc_attr($event_registration_link['target']); ?>"<?php endif; ?>>
-                                                            <?php echo esc_html($event_registration_link['title'] ?: 'SIGN UP!'); ?>
-                                                        </a>
-                                                    <?php else: ?>
-                                                        <a href="#" class="register-link">SIGN UP!</a>
-                                                    <?php endif; ?>
+                                                <div class="event-detail-item">
+                                                    <div class="detail-label">ATTENDANCE</div>
+                                                    <div class="detail-value">
+                                                        <?php
+                                                        $event_attendance_info = get_field('event_attendance_info') ?: get_field('attendance_text') ?: 'The event is free, but seating is limited to 50.';
+                                                        $event_registration_link = get_field('event_registration_link') ?: get_field('signup_button_link');
+                                                        ?>
+                                                        <?php echo nl2br(esc_html($event_attendance_info)); ?><br>
+                                                        <?php if ($event_registration_link): ?>
+                                                            <a href="<?php echo esc_url($event_registration_link['url']); ?>"
+                                                               class="book-tickets-btn"
+                                                               <?php if ($event_registration_link['target']): ?>target="<?php echo esc_attr($event_registration_link['target']); ?>"<?php endif; ?>>
+                                                                <?php echo esc_html($event_registration_link['title'] ?: 'BOOK TICKETS'); ?>
+                                                            </a>
+                                                        <?php else: ?>
+                                                            <a href="#" class="book-tickets-btn">BOOK TICKETS</a>
+                                                        <?php endif; ?>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
