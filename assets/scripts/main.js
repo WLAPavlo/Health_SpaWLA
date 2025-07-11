a0e6a2fba515ce4f5949ba9c503d5956ca4326ff
Pavlo <pavlo.lu@thewhitelabelagency.com>
2025-07-11 18:29:18 +0300

    Commit Process

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -265,7 +265,62 @@ function render_map($el) {
     center: new google.maps.LatLng(0, 0),
     mapTypeId: google.maps.MapTypeId.ROADMAP,
     scrollwheel: false,
-    // styles : styles // Uncomment for map styling
+    styles: [
+      {
+        featureType: 'all',
+        elementType: 'labels.text',
+        stylers: [
+          {
+            color: '#878787',
+          },
+        ],
+      },
+      {
+        featureType: 'all',
+        elementType: 'labels.text.stroke',
+        stylers: [
+          {
+            visibility: 'off',
+          },
+        ],
+      },
+      {
+        featureType: 'landscape',
+        elementType: 'all',
+        stylers: [
+          {
+            color: '#f9f5ed',
+          },
+        ],
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'all',
+        stylers: [
+          {
+            color: '#f5f5f5',
+          },
+        ],
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'geometry.stroke',
+        stylers: [
+          {
+            color: '#c9c9c9',
+          },
+        ],
+      },
+      {
+        featureType: 'water',
+        elementType: 'all',
+        stylers: [
+          {
+            color: '#aee0f4',
+          },
+        ],
+      },
+    ],
   };
 
   // create map
